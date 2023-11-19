b776a7836779952690098efb09d38ae4db1d2ab4
Juan <133364350+JuanGimenes06@users.noreply.github.com>
2023-11-18 23:49:32 -0300

    Em casa
    terminei as animações, transições e estilo da tela de carregamento;
    
    Fiz e animei por cima (talvez provisório) o carrossel de imagem dos esboços;
    
    estilizei a barra de rolagem

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// Tela de carregamento
+
 let frases = [
-    "",
+    "Carregando",
     "Preparando para a criação",
     "Pegando o Martelo e a Bigorna",
     "Forjando uma nova estrela",
@@ -8,29 +10,32 @@ let frases = [
     "Estrela forjada",
     "Ei Geppetto sai da tela de Loading!!!",
     "Você tá realmente lendo isso?",
-    "Tudo Pronto!"
-
+    "Tudo Pronto!",
 ];
+
+let telaCarregamento = document.querySelector(".telaCarregamento");
+var header = document.getElementById("header");
+var main = document.getElementById("main");
+var footer = document.getElementById("footer");
 
 let barraProgresso = document.querySelector('#barraProgresso');
 let textoCarregamento = document.querySelector('.textoCarregamento');
 let divProgresso = document.querySelector('#progresso');
-let telaCarregamento = document.querySelector(".telaCarregamento");
-let estrelasPequenas = document.querySelectorAll('.starPequena');
+const textoLogoAnimacao = document.querySelector('.texto-logo');
 
 let progresso = 0;
 let posicao = 0;
 let contador = 0;
 
+
 function trocarConteudo() {
     textoCarregamento.classList.add('fadeOut');
     setTimeout(() => {
-        textoCarregamento.innerHTML = frases[contador];
-        console.log(contador)
+        let contadorFrases = contador - 1;
+        textoCarregamento.innerHTML = frases[contadorFrases];
         textoCarregamento.classList.remove('fadeOut');
         
-    }, 500); 
-}
+    }, 500); }
 
 function atualizarContador() {
 
@@ -42,12 +47,10 @@ function atualizarContador() {
 
     if (contador === 10) {
         pausarContador();
-        telaCarregamento.style.display = 'none';
-        document.getElementById("header").style.display = 'block';
-        document.getElementById("main").style.display = 'block';
-        document.getElementById("footer").style.display = 'block';
-    }
-}
+
+        setTimeout(terminoContador, 6000)
+        setTimeout(mostrarSite, 9000)
+    } }
 
 function pausarContador() {
     clearInterval(posicao);
@@ -56,7 +59,61 @@ function pausarContador() {
 function iniciarContador() {
     if (contador !== 10) {
         posicao = setInterval(atualizarContador, 5000);
-    }   
+}}
+
+function terminoContador(){
+
+    setTimeout( () => {
+        barraProgresso.style.display = 'none';
+        divProgresso.style.display = 'none';
+        textoCarregamento.style.display = 'none';
+
+        const check = document.getElementById('check');
+        const iconCheck = document.createElement('div');
+    
+        iconCheck.classList.add('iconCheck');
+        iconCheck.innerHTML = `
+        <i class="fa-sharp fa-regular fa-circle-check fa-fade fa-2xl"></i>
+        `;
+
+        check.appendChild(iconCheck);
+        check.style.opacity = 1;
+    }, 1000)
+
+    
+    barraProgresso.style.opacity = 0;
+    textoCarregamento.style.opacity = 0;
+    
+}
+
+function mostrarSite(){
+
+  // Adiciona estilo para animar a tela de carregamento
+    telaCarregamento.style.opacity = "0";
+    telaCarregamento.style.height = "0";
+
+  // Adiciona estilo para animar o header
+    header.style.opacity = "1";
+    header.style.height = "auto";
+    header.style.transform = "translateY(100%)"; // Começa abaixo da viewport
+
+    textoLogoAnimacao.style.animationName = 'fontalicious';
+    textoLogoAnimacao.style.animationIterationCount = '4';
+    textoLogoAnimacao.style.animationDuration = '1s';
+
+  // Adiciona estilo para animar o main e o footer
+    main.style.opacity = "1";
+    main.style.height = "auto";
+
+    footer.style.opacity = "1";
+    footer.style.height = "auto";
+
+    setTimeout(function() {
+        header.style.transition = "transform 1s ease-in-out, opacity 1s ease-in-out";
+        header.style.transform = "translateY(0)"; // Move para a posição inicial (acima da viewport)
+      }, 10); // Ajuste conforme necessário
+
+
 }
 
 document.querySelector('#carregar').addEventListener("click", function () {
@@ -67,3 +124,55 @@ document.querySelector('#carregar').addEventListener("click", function () {
 
     iniciarContador();
 });
+
+// Carrosel 
+
+const IMAGENS = [
+    'img/imagem1.png', 
+    'img/imagem2.png', 
+];
+
+let imgIndice = 0;
+const IMG = document.getElementById('carrosselImagem');
+const bolinhas = document.querySelectorAll('.bolinha');
+const progressoCarrossel = document.querySelectorAll('.progressoCarrossel')
+
+function carregarImagem() {
+    IMG.style.opacity = 0; // Inicia a transição (fade-out)
+    resetarBolinhas(); // Reseta todas as bolinhas
+    bolinhas[imgIndice].classList.add('ativa'); // Ativa a bolinha correspondente
+
+    setTimeout(() => {
+        IMG.src = IMAGENS[imgIndice];
+        IMG.style.opacity = 1; // Conclui a transição (fade-in)
+    }, 500); // Tempo de espera para a transição
+}
+
+function resetarBolinhas() {
+    bolinhas.forEach(bolinha => {
+        bolinha.classList.remove('ativa');
+    });
+}
+
+function passarIMG() {
+    var limite = IMAGENS.length - 1;
+
+    if (imgIndice < limite) {
+        ++imgIndice;
+        carregarImagem();
+    } else {
+        imgIndice = 0;
+        carregarImagem();
+    }
+}
+
+// Adiciona eventos de clique para as bolinhas
+bolinhas.forEach((bolinha, index) => {
+    bolinha.addEventListener('click', () => {
+        imgIndice = index;
+        carregarImagem();
+    });
+});
+
+
+setInterval(passarIMG, 8000)
